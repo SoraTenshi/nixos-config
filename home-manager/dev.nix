c08fc10f45f6da8dec06b7ed06b690a051658cdc
Sora <dream@neoncity.dev>
2021-12-02 20:38:27 +0100

    Create README.md

--- home-manager/dev.nix
+++ home-manager/dev.nix
@@ -4,9 +4,37 @@
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-    spacevim
+    neovim
     vscode
   ];
 
+  programs.neovim = {
+    enable = true;
+    coc.enable = true;
+    extraConfig = ''
+      set clipboard = unnamedplus
+      syntax on
+      set backspace = 2
+
+      set visualbell
+      set t_vb=
+
+      set relativenumber
+      set number
+
+      set ruler
+
+      set tabstop=2 smarttab
+      set cursorline
+    '';
+    plugins = with pkgs.vimPlugins; [
+      telescope-nvim
+      nvim-treesitter
+      lsp_signature-nvim
+      nvim-compe
+      vim-easy-align
+      vim-multiple-cursors
+    ];
+  };
 
 }
