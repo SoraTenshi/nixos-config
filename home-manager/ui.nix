bf89593286c8bed7fdd6987d36edd6ae58872406
s0LA1337 <dreamer@neoncity.dev>
2021-12-12 03:35:07 +0100

    picom

--- home-manager/ui.nix
+++ home-manager/ui.nix
@@ -3,22 +3,7 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the usage of home-manager
-  # The point of home-manager is to create
-  # symlinks from /nix/store
-  program.home-manager.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    displayManager.defaultSession = "none+leftwm";
-    windowManager.leftwm.enable = true; 
-    windowManager.leftwm.startup = [
-      { command = "systemctl --user restart polybar"; always = true; notification = false; }
-    ];
-  };
-
-  services.polybar.enable = true;
+  programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
     # App starter
@@ -28,9 +13,32 @@
     # in the future probably penrose
     leftwm
 
+    # Compositor
+    picom
+
     # Panel
     polybar
   ];
 
+  services.picom = {
+    enable = true;
+    fade = true;
+    fadeDelta = 5;
+    shadow = true;
+    shadowOpacity = "0.5";
+  };
 
+  # leftwm kinda takes cares of the polybar :)
+  home.file = {
+    ".config/leftwm" = {
+      source = let
+        repo = pkgs.fetchFromGitHub {
+          owner  =  "s0la1337";
+          repo   =  "dotfiles";
+          rev    =  "81dfe16c977ed296b8466e7e528281ff33aaa464";
+          sha256 =  "145538jm473ygm5paxmikzs1z98qj4lswm6iild8cpv0w31lk55h";
+        };
+      in "${repo}/leftwm/";
+    };
+  };
 }
