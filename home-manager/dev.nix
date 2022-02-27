c1a1b66782fff06d12286d1c55e27e84e5a2d78b
s0LA1337 <dream@neoncity.dev>
2022-02-27 21:23:13 +0000

    Use required lua

--- home-manager/dev.nix
+++ home-manager/dev.nix
@@ -43,6 +43,7 @@
 
     extraConfig = ''
       lua require('indent')
+      lua require('completion')
 
       nnoremap <leader>ff <cmd>Telescope find_files<cr>
       nnoremap <leader>fb <cmd>Telescope buffers<cr>
