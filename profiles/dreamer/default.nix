ab1e0527eda38f410cd358ed60e25e3ea3f1b070
s0LA1337 <dream@neoncity.dev>
2022-03-06 23:34:22 +0000

    Adapted to use flakes

--- profiles/dreamer/default.nix
+++ profiles/dreamer/default.nix
@@ -8,25 +8,26 @@
     extraGroups = [ "wheel" ];
 
     shell = pkgs.zsh;
+  };
 
-    home-manager.users."dreamer" = { pkgs, nixpkgs, ...}: {
-      imports = [
-        ./alacritty.nix
-        ./userApps.nix
-        ./x11.nix
+  home-manager.users."dreamer" = { pkgs, nixpkgs, ...}: {
+    imports = [
+      ./alacritty.nix
+      ./userApps.nix
+      ./x11.nix
 
-        # General purpose
-        ../../general_purpose/nvim.nix
-        ../../general_purpose/zsh.nix
+      # General purpose
+      ../../general_purpose/nvim.nix
+      ../../general_purpose/zsh.nix
 
-        # development
-        ../../development/c.nix
-        ../../development/zig.nix
-        ../../development/rust.nix
-        ../../development/misc.nix
-      ];
+      # development
+      ../../development/c.nix
+      ../../development/zig.nix
+      ../../development/rust.nix
+      ../../development/misc.nix
+    ];
 
-      home.stateVersion = "21.11";
-      home.keyboard.layout = "us";
-    }
+    home.stateVersion = "21.11";
+    home.keyboard.layout = "us";
+  };
 }
