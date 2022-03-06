f63d34fd440e658557c490b8bbc91209094bbb64
s0LA1337 <dream@neoncity.dev>
2022-03-06 23:34:22 +0000

    Adapted to use flakes

--- flake.nix
+++ flake.nix
@@ -2,12 +2,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
+ #   dotfiles.url = "github:s0la1337/dotfiles";
   };
 
   outputs = { self, nixpkgs, home-manager }@inputs: {
-    nixosConfigurations.neoncity = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+    nixosConfigurations = {
+      wsl = nixpkgs.lib.nixosSystem { 
+        system = "x86_64-linux";
+        modules = [ 
+          ./profiles/wsl/default.nix 
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+      neoncity = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+          ./profiles/neoncity/default.nix 
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
     };
   };
 }
--- profiles/wsl/default.nix
+++ profiles/wsl/default.nix
@@ -1,28 +1,17 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  users.users."nixos" = {
-    isNormalUser = true;
-    initialPassword = "12345";
-    group = "users";
-    extraGroups = [ "wheel" ];
+  home-manager.users."nixos" = { pkgs, nixpkgs, ...}: {
+    imports = [
+      # General purpose
+      ../../general_purpose/nvim.nix
+      ../../general_purpose/zsh.nix
 
-    shell = pkgs.zsh;
-
-    home-manager.users."nixos" = { pkgs, nixpkgs, ...}: {
-      imports = [
-        # General purpose
-        ../../general_purpose/nvim.nix
-        ../../general_purpose/zsh.nix
-
-        # development
-        ../../development/c.nix
-        ../../development/zig.nix
-        ../../development/rust.nix
-        ../../development/misc.nix
-      ];
-
-      home.stateVersion = "21.11";
-      home.keyboard.layout = "us";
-    }
+      # development
+      ../../development/c.nix
+      ../../development/zig.nix
+      ../../development/rust.nix
+      ../../development/misc.nix
+    ];
+  };
 }
