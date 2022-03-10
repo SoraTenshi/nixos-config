4d628dcada2c7c71d542fff08e7f09e358494c1e
s0LA1337 <dream@neoncity.dev>
2022-03-10 18:00:18 +0000

    Restructure nix config to be more accessible

--- profiles/dreamer/default.nix
+++ profiles/dreamer/default.nix
@@ -1,37 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "neoncity";
-  networking.networkmanager.enable = true;
-
-  sound.enable = true;
-
-  time.timeZone = "Europe/Berlin";
-
-  fonts.fonts = with pkgs; [
-    jetbrains-mono
-    meslo-lgs-nf
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
-
-  users.users."dreamer" = {
-    isNormalUser = true;
-    initialPassword = "12345";
-    group = "users";
-    extraGroups = [ "wheel" "networkmanager" ];
-
-    shell = pkgs.zsh;
-  };
-
   home-manager.users."dreamer" = { pkgs, nixpkgs, ...}: {
     imports = [
       # User applications
-      ./alacritty.nix
-      ./userApps.nix
-      ./x11.nix
+      ../../applications/alacritty.nix
+      ../../applications/user_apps.nix
+      ../../applications/x11.nix
 
       # General purpose
       ../../general_purpose/nvim.nix
@@ -43,8 +18,5 @@
       ../../development/rust.nix
       ../../development/misc.nix
     ];
-
-    home.stateVersion = "21.11";
-    home.keyboard.layout = "us";
   };
 }
