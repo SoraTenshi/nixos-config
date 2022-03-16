c8feb9b9d5026986d3249f812cf892afc7943b28
s0LA1337 <dream@neoncity.dev>
2022-03-16 22:24:33 +0100

    Fixed all issues with thinkpad

--- applications/user_apps.nix
+++ applications/user_apps.nix
@@ -2,13 +2,9 @@
 {
   programs.home-manager.enable = true;
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "discord"
-    "vivaldi"
-    "steam"
-  ];
-
   home.packages = with pkgs; [
+    discord vivaldi steam
+
     # Screenshots!
     flameshot
 
