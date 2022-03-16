5dc72f64c62575d1316a8d7f590e4ea9db6d3a84
s0LA1337 <dream@neoncity.dev>
2022-03-16 20:20:14 +0000

    Fix typos

--- applications/user_apps.nix
+++ applications/user_apps.nix
@@ -1,14 +1,8 @@
-# Used for the configuration of specific configurations
-
 { config, pkgs, ... }:
-
 {
-  # Enable the usage of home-manager
-  # The point of home-manager is to create
-  # symlinks from /nix/store
   programs.home-manager.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
+  config.allowUnfree = true;
 
   home.packages = with pkgs; [
     # Well... of course!
