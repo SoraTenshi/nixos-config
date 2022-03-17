20166f2269e27552f304ca22e517c5b0cd5ddf99
s0LA1337 <dream@neoncity.dev>
2022-03-17 20:59:12 +0100

    Flake update; simple river config, restructure x11

---
+++ applications/wayland/river.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ...}:
+{
+
+  home.packages = with pkgs; [
+    # Compositor
+    river
+
+    # App launcher
+    wofi
+
+    # Status bar
+    yambar
+
+    # Power management panel
+    wlogout
+
+    # Lockscreen
+    swaylock
+
+    # Layout generator
+    rivercarro
+
+    # Wallpaper stuff
+    oguri
+  ];
+
+  hardware.opengl.enable = true;
+
+  programs.xwayland.enable = true;
+
+}
