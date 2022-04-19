493040bb5a35ac4567c314f6ecce4ddd32432d0d
s0LA1337 <dream@neoncity.dev>
2022-04-19 23:14:06 +0000

    Update

--- development/misc.nix
+++ development/misc.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    swift
-    
     go
 
     pythonFull
--- development/tools.nix
+++ development/tools.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    lazygit
+    lazygit git
   ];
 }
