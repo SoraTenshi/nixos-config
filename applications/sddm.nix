6345d57134be953c4f1d66243661ea12207a25c6
s0LA1337 <dream@neoncity.dev>
2022-05-04 18:34:28 +0000

    Added sddm

--- applications/sddm.nix
+++ applications/sddm.nix
@@ -1,4 +1,16 @@
-{ config, pkgs, sddm-theme, dotfiles, ... }:
+{ config, pkgs, sddm-theme, ... }:
 {
-  
+  services.xserver.displayManager = {
+    defaultSession = "none+leftwm";
+    sddm = {
+      enable = true;
+      theme = "tokyo-night-sddm";
+    };
+  };
+
+  home.file = {
+    "/usr/share/sddm/themes" = {
+      source = "${sddm-theme}";
+    };
+  };
 }
