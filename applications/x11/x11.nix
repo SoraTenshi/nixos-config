7705f639bf7e245357316689b8c5b7d8a29695b3
s0LA1337 <dream@neoncity.dev>
2022-05-08 17:23:56 +0000

    Attempt to fix up polybar creation

--- applications/x11/x11.nix
+++ applications/x11/x11.nix
@@ -42,5 +42,11 @@
     ".config/leftwm" = {
       source = "${dotfiles}/leftwm";
     };
+    ".config/polybar" = {
+      source = "${dotfiles}/polybar";
+    };
+    ".config/leftwm/themes/current" = {
+      source = config.lib.file.mkOutOfStoreSymlink ".config/polybar/tokyo-night/night";
+    };
   };
 }
