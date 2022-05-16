a3f56af4fb733c49f52246fed0efed35c4f5a77e
s0LA1337 <dream@neoncity.dev>
2022-05-16 21:45:44 +0200

    Remove shadows

--- applications/x11/x11.nix
+++ applications/x11/x11.nix
@@ -27,13 +27,25 @@
     enable        = true;
     fade          = true;
     fadeDelta     = 5;
-    shadow        = true;
-    shadowOpacity = "0.5";
+    shadow        = false;
     blur          = true;
     extraOptions  = ''
       # Corners
       corner-radius = 12.0;
       round-borders = 1;
+
+      blur: {
+        method = "dual_kawase";
+        strength = 6;
+        background = true;
+        background-frame = false;
+        background-fixed = false;
+        kern = "5x5box";
+      }
+
+      blur-background = false;
+      blur-background-frame = true;
+      blur-background-fixed = true;
     '';
   };
 
