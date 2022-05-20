745838668361e5b32112bb8a9b5b62c8da97e4f5
s0LA1337 <dreamer@neoncity.dev>
2022-05-20 23:29:44 +0000

    yep
    s

--- profiles/wsl/default.nix
+++ profiles/wsl/default.nix
@@ -6,7 +6,6 @@
     ../../general_purpose/nvim.nix
     ../../general_purpose/zsh.nix
     ../../general_purpose/misc.nix
-    ../../general_purpose/emacs.nix
 
     # development
     ../../development/developing.nix
