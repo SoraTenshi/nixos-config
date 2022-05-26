918c19cbe199beb6b0a78c768ccd4e379ee3a6f7
s0LA1337 <dreamer@neoncity.dev>
2022-05-26 00:52:12 +0000

    Better

--- general_purpose/nvim.nix
+++ general_purpose/nvim.nix
@@ -18,7 +18,7 @@
       # buffer stuff
       bufferline-nvim
       # auto complete
-      nvim-cmp cmp-buffer cmp-path lspkind-nvim nvim-lspconfig lsp_signature-nvim cmp_luasnip
+      nvim-cmp cmp-buffer cmp-path lspkind-nvim nvim-lspconfig lsp_signature-nvim cmp_luasnip luasnip
       # syntax highlighting
       vim-polyglot
       # design stuff
