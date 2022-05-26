ad0dc907e72286345238ec8ca7cbcb8d281b454e
s0LA1337 <dreamer@neoncity.dev>
2022-05-26 01:06:24 +0000

    Somehow made lsp work. Yay!

--- general_purpose/nvim.nix
+++ general_purpose/nvim.nix
@@ -18,7 +18,7 @@
       # buffer stuff
       bufferline-nvim
       # auto complete
-      nvim-cmp cmp-buffer cmp-path lspkind-nvim nvim-lspconfig lsp_signature-nvim cmp_luasnip luasnip
+      nvim-cmp cmp-buffer cmp-path lspkind-nvim nvim-lspconfig lsp_signature-nvim cmp_luasnip luasnip cmp-nvim-lsp
       # syntax highlighting
       vim-polyglot
       # design stuff
