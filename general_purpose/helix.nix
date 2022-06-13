d46bc4fd4d7e4f3c43c5916b6c80924267ce8176
s0LA1337 <dreamer@neoncity.dev>
2022-06-13 15:33:53 +0000

    Embed helix file in helix.nix

--- general_purpose/helix.nix
+++ general_purpose/helix.nix
@@ -1,18 +1,57 @@
 { pkgs, dotfiles, ... }: 
 {
   home.packages = with pkgs; [
+    # Helix as main package
     helix
 
-    ccls gopls rnix-lsp rust-analyzer zls 
-    omnisharp-roslyn sumneko-lua-language-server 
-
+    # Language servers
+    ccls 
+    gopls 
+    rnix-lsp 
+    rust-analyzer 
+    zls 
+    omnisharp-roslyn 
+    sumneko-lua-language-server 
     nodePackages.vim-language-server
     haskell-language-server
   ];
 
   home.file = {
-    ".config/helix" = {
-      source = "${dotfiles}/helix";
+    ".config/helix/config.toml" = {
+      source = ''
+theme = "tokyonight_storm"
+
+[editor]
+line-number = "relative"
+scrolloff = 10
+idle-timeout = 200
+true-color = true
+
+[editor.cursor-shape]
+insert = "bar"
+
+[editor.auto-pairs]
+'(' = ')'
+'{' = '}'
+'[' = ']'
+'"' = '"'
+'`' = '`'
+'<' = '>'
+
+[editor.whitespace]
+render = "all"
+
+[editor.whitespace.characters]
+space = "·"
+nbsp = "⍽"
+tab = "→"
+newline = "⏎"
+      '';
     };
-  };
+    ".config/helix/language.toml" = {
+      source = ''
+use-grammars = { }
+      '';
+    };
+   };
 }
