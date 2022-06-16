1af275ba0cb97f4685038117b332cfdb2f715906
s0LA1337 <dreamer@neoncity.dev>
2022-06-16 01:35:36 +0000

    test

--- ui/x11/xmonad.nix
+++ ui/x11/xmonad.nix
@@ -1,6 +1,14 @@
 { config, pkgs, dotfiles, ... }:
 
 {
+  home.pointerCursor = {
+    package = pkgs.nordzy-cursor-theme;
+    name = "Nordzy-black-cursors";
+    size = 32;
+    x11.enable = true;
+    gtk.enable = true;
+  };
+  
   home.packages = with pkgs; [
     # App starter
     rofi rofi-power-menu rofi-pulse-select rofi-file-browser
