76d63dd75ee4be11d5445fd000ef8ac4570ac39a
s0LA1337 <dream@neoncity.dev>
2022-07-02 14:31:35 +0000

    adjust font-patch.nix
    a
    
    too lazy to do actual commits
    
    more fixes
    
    a

--- custom/font-patch.nix
+++ custom/font-patch.nix
@@ -2,54 +2,32 @@
   description = "An attempt to patch any font with the nerdfont-patcher";
 
   inputs = {
-    font-name = {
-      flake = false;
-      value = "";
-    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, font-name }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
       allSystems = flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = import <nixpkgs> {};
-            unmodifiedPackage = pkgs.font-name.override;
-
-            nerdFontPatched =
-              let outputDir = "/usr/share/fonts/truetype";
-              in
-              pkgs.stdenv.mkDerivation {
-                pname = font-name ++ "nerd-font";
-                version = unmodifiedPackage.version;
-                src = pkgs."${font-name.value}".src;
-                buildInputs = [ pkgs.nerd-font-patcher ];
-                configure = "mkdir -p ${outputDir}";
-                build = ''
-                  for file in ${unmodifiedPackage}/share/fonts/truetype/*"
-                  do
-                  nerd-font-patcher $file --complete --outputdir ${outputDir}
-                  done
-                '';
-              };
-
-            packages = {
-              normal = unmodifiedPackage;
-              nerd-font = nerdFontPatched;
-            };
-          in
-          {
-            inherit packages;
-            defaultPackage = unmodifiedPackage;
-          }
-        );
+        (system: {
+          defaultPackages = allSystems.defaultPackage;
+        });
+      outputDir = "/usr/share/fonts/truetype";
     in
     {
-      packages = allSystems.packages;
-      defaultPackages = allSystems.defaultPackage;
       overlay = final: prev: {
-        font-name.value = allSystems.packages.${final.system};
+        mkNerdFont = { font-drv }: prev.stdenv.mkDerivation {
+          pname = font-drv.pname ++ "nerd-font";
+          version = font-drv.version;
+          src = font-drv.src;
+          nativeBuildInputs = [ prev.nerd-font-patcher ];
+          configure = "mkdir -p ${outputDir}";
+          build = ''
+            for file in ${font-drv}/share/fonts/truetype/*"
+            do
+            nerd-font-patcher $file --complete --outputdir ${outputDir}
+            done
+          '';
+        };
       };
     };
 }
