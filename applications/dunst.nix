1aaf5e4b335fa404b95bcc99ff5bd9512c143cf3
s0LA1337 <root@Sorer.localdomain>
2022-07-27 17:01:18 +0000

    Add dunst.nix

--- applications/dunst.nix
+++ applications/dunst.nix
@@ -1,6 +1,40 @@
 { pkgs, ...}:
 {
-  home.packages = with pkgs; [
-    dunst
-  ];
+  services.dunst = {
+    enable = true;
+    
+    settings = {
+      global = {
+        alignment = "left";
+        vertical_alignment = "center";
+        width = 400;
+        height = 500;
+        offset = "50x50";
+        origin = "top-right";
+        transparency = 30;
+        frame_color = "#1a1b26"; # non storm for better frame-contrast :)
+        scale = 0;
+        font = "RictyDiminished with FiraCode 11";
+        
+        padding = 8;
+        horizontal_padding = 8;
+        text_icon_padding = 0;
+        
+        progress_bar = true;
+        progress_bar_height = 10;
+        progress_bar_frame_width = 1;
+        progress_bar_min_width = 150;
+        progress_bar_max_width = 300;
+        
+        seperator_color = "frame";
+        sort = "yes";
+      };
+      
+      urgency_normal = {
+        background = "#24283b";
+        foreground = "#a9b1d6";
+        timeout = 7;
+      };
+    };
+  };
 }
