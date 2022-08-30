9678537fc7bad33d5fada31fd5a32872e3e3fa51
s0LA1337 <dream@neoncity.dev>
2022-08-31 00:00:40 +0200

    Up
    up

--- env/bootable.nix
+++ env/bootable.nix
@@ -4,10 +4,6 @@ let
   defaultUser = "dreamer";
 in
 {
-  imports = [
-    ../ui/x11/x11.nix
-  ];
-
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
@@ -48,28 +44,28 @@ in
   hardware.pulseaudio.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  services.xserver = {
-    enable = true;
-    displayManager.defaultSession = "none+leftwm";
-    desktopManager.runXdgAutostartIfNone = true;
-    windowManager.leftwm.enable = true;
-    # touchpad support!
-    libinput.enable = true;
-  };
-
   # services.xserver = {
   #   enable = true;
-  #   displayManager.defaultSession = "none+xmonad";
+  #   displayManager.defaultSession = "none+leftwm";
   #   desktopManager.runXdgAutostartIfNone = true;
-  #   windowManager.xmonad = {
-  #     enable = true;
-  #     enableContribAndExtras = true;
-  #     extraPackages = haskellPackages: [
-  #       pkgs.xmonad-log
-  #     ];
-  #     config = ../xmonad/xmonad.hs;
-  #   };
+  #   windowManager.leftwm.enable = true;
+  #   # touchpad support!
+  #   libinput.enable = true;
   # };
+
+  services.xserver = {
+    enable = true;
+    displayManager.defaultSession = "none+xmonad";
+    desktopManager.runXdgAutostartIfNone = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+        pkgs.xmonad-log
+      ];
+      config = ../xmonad/xmonad.hs;
+    };
+  };
 
   programs.ssh.startAgent = true;
 
