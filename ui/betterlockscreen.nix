aaf4a9865d3bebb009ffef97f752ac6fe2fc16b0
s0LA1337 <dream@neoncity.dev>
2022-08-31 21:54:50 +0200

    Update i guess

--- ui/betterlockscreen.nix
+++ ui/betterlockscreen.nix
@@ -2,7 +2,7 @@
 {
   services.betterlockscreen = {
     enable = true;
-    arguments = [ "--blur 0.5" ];
+    arguments = [ "--blur 0.5" "-l" "--dim 60" ];
     inactiveInterval = 10;
   };
 }
--- ui/x11/picom.nix
+++ ui/x11/picom.nix
@@ -14,10 +14,10 @@
         background = true;
         background-frame = false;
         background-fixed = false;
-        kern = "5x5box";
+        kern = "3x3box";
       };
 
-      blur-background = false;
+      blur-background = true;
       blur-background-frame = true;
       blur-background-fixed = true;
     };
