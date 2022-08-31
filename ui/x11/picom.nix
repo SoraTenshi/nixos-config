37354c0b55867be32ea747bec8587eb247862a74
s0LA1337 <dream@neoncity.dev>
2022-08-31 23:56:11 +0200

    Commit
    a

--- ui/x11/picom.nix
+++ ui/x11/picom.nix
@@ -4,22 +4,20 @@
     enable = true;
     fade = true;
     fadeDelta = 5;
-    shadow = false;
+    experimentalBackends = true;
+
     settings = {
       corner-radius = 10.0;
+      rounded-corners-exclude = [ "class_g = 'eww'" ];
 
       blur = {
-        method = "dual_kawase";
-        strength = 2;
+        method = "kawase";
+        strength = 5;
         background = true;
         background-frame = false;
         background-fixed = false;
-        kern = "3x3box";
+        kern = "5x5box";
       };
-
-      blur-background = true;
-      blur-background-frame = true;
-      blur-background-fixed = true;
     };
   };
 }
