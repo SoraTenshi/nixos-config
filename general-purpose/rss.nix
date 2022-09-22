3c3e8f44730643d715958da3f691f5869a9c1e43
s0LA1337 <dream@neoncity.dev>
2022-09-22 19:31:02 +0000

    Update newsboat urls

--- general-purpose/rss.nix
+++ general-purpose/rss.nix
@@ -4,8 +4,36 @@
     enable = true;
     urls = [
       {
-        url =  "";
-        title = "";
+        url = "http://blog.fefe.de/rss.xml?html";
+        title = "Fefe's Blog";
+      }
+      {
+        url = "https://www.microsoft.com/en-us/research/feed/";
+        title = "Microsoft Research";
+      }
+      {
+        url = "https://research.googleblog.com/feeds/posts/default?alt=rss";
+        title = "Google Research";
+      }
+      {
+        url = "https://github.blog/feed/";
+        title = "GitHub Blog";
+      }
+      {
+        url = "https://github.blog/category/engineering/feed/";
+        title = "GitHub Engineering";
+      }
+      {
+        url = "https://ziglang.org/news/index.xml";
+        title = "Ziglang News";
+      }
+      {
+        url = "https://zig.news/rss";
+        title = "zig.news";
+      }
+      {
+        url = "https://devblogs.microsoft.com/oldnewthing/feed";
+        title = "old new thing";
       }
     ];
     maxItems = 20;
