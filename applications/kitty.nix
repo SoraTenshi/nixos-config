72877011542b392481d992fad9c5478ea6815681
s0LA1337 <dream@neoncity.dev>
2022-09-24 01:04:56 +0000

    Update i guess?

--- applications/kitty.nix
+++ applications/kitty.nix
@@ -5,26 +5,46 @@
     enable = true;
     font = {
       name = "MesloLGS NF";
+      size = 11;
+    };
+    keybindings = {
+      "ctrl+shift+c" = "copy_to_clipboard";
+      "ctrl+shift+v" = "paste_from_clipboard";
+      "" = "";
+      "" = "";
+      "" = "";
+      "" = "";
+    };
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+      
+      # Tabs
+      tab_bar_edge = "bottom";
+      tab_bar_style = "fade";
+      tab_bar_align = "center";
+      tab_seperator = "｜";
     };
     theme = "Tokyo Night Storm";
   };
   
-      shell.program = "${pkgs.zsh}/bin/zsh";
-      window.opacity = 0.8;
-      draw_bold_text_with_bright_colors = true;
-      font = {
-        size = 11;
-        normal = {
-          family = "MesloLGS NF";
-          style = "Regular";
-        };
-        bold = {
-          family = "MesloLGS NF";
-          style = "Bold";
-        };
-        italic = {
-          family = "MesloLGS NF";
-          style = "Italic";
-    };
-  };
+  #     shell.program = "${pkgs.zsh}/bin/zsh";
+  #     window.opacity = 0.8;
+  #     draw_bold_text_with_bright_colors = true;
+  #     font = {
+  #       size = 11;
+  #       normal = {
+  #         family = "MesloLGS NF";
+  #         style = "Regular";
+  #       };
+  #       bold = {
+  #         family = "MesloLGS NF";
+  #         style = "Bold";
+  #       };
+  #       italic = {
+  #         family = "MesloLGS NF";
+  #         style = "Italic";
+  #   };
+  # };
 }
