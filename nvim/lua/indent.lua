83ce35d33df99bd5f70f358a4aadfdef2fb6b86f
s0LA1337 <dream@neoncity.dev>
2022-09-30 17:15:53 +0000

    Cleanup

--- nvim/lua/indent.lua
+++ nvim/lua/indent.lua
@@ -12,13 +12,13 @@ vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
+  space_char_blankline = " ",
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+  },
 }
--- nvim/lua/init_ll.lua
+++ nvim/lua/init_ll.lua
@@ -2,27 +2,27 @@ local config = {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
+    component_separators = { '', '' },
+    section_separators = { '', '' },
     disabled_filetypes = {}
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'filename'},
+    lualine_a = { 'mode' },
+    lualine_b = { 'filename' },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {'encoding', 'fileformat', 'filetype'},
-    lualine_z = {{'branch'}, {'progress'}},
+    lualine_y = { 'encoding', 'fileformat', 'filetype' },
+    lualine_z = { { 'branch' }, { 'progress' } },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {},
-    lualine_x = {'location'},
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = { },
+  tabline = {},
   extensions = {}
 }
 
@@ -37,21 +37,21 @@ local function ins_right(component)
 end
 
 ins_left {
-	'lsp_progress',
-	display_components = { 'lsp_client_name', { 'title', 'percentage', 'message' }},
-	-- With spinner
-	-- display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' }},
-	separators = {
-		component = ' ',
-		progress = ' | ',
-		message = { pre = '(', post = ')'},
-		percentage = { pre = '', post = '%% ' },
-		title = { pre = '', post = ': ' },
-		lsp_client_name = { pre = '[', post = ']' },
-		spinner = { pre = '', post = '' },
-	},
-	timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-	message = { commenced = 'In Progress', completed = 'Completed' },
+  'lsp_progress',
+  display_components = { 'lsp_client_name', { 'title', 'percentage', 'message' } },
+  -- With spinner
+  -- display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' }},
+  separators = {
+    component = ' ',
+    progress = ' | ',
+    message = { pre = '(', post = ')' },
+    percentage = { pre = '', post = '%% ' },
+    title = { pre = '', post = ': ' },
+    lsp_client_name = { pre = '[', post = ']' },
+    spinner = { pre = '', post = '' },
+  },
+  timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+  message = { commenced = 'In Progress', completed = 'Completed' },
 }
 
 require('lualine').setup(config)
