c180e64be633cf3f4f8032e58df957be12eae18a
s0LA1337 <dream@neoncity.dev>
2022-10-02 19:11:15 +0000

    Update and cleanup unused stuff

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
     zig-master.url = "github:mitchellh/zig-overlay";
     helix-master.url = "github:helix-editor/helix";
 
-    # font-patcher.url   = "github:s0la1337/nerd-font-patcher-overlay"; # makes some trouble :(
-    # nur.url          = "github:nix-community/NUR"; # not used yet, but make it accessible when i need to use it.
-
-    # flake-compat = {
-    #   url = "github:edolstra/flake-compat";
-    #   flake = false;
-    # };
-
     picom-ibhagwan = {
       url = "github:ibhagwan/picom";
       flake = false;
@@ -79,26 +71,6 @@
           ];
         };
 
-        wsl-colemak = nixpkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-            ./machines/wsl/wsl.nix
-            ./development/global.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit
-                  neovim-nightly zig-master helix-master;
-              };
-              home-manager.users.dreamer = { ... }: {
-                imports = [ ./profiles/wsl-colemak/default.nix ];
-              };
-            }
-          ];
-        };
-
         t470 = nixpkgs.lib.nixosSystem {
           system = system;
           specialArgs = { inherit sddm-theme grub2-theme; };
@@ -123,7 +95,7 @@
           ];
         };
 
-        neoncity = nixpkgs.lib.nixosSystem {
+        t420 = nixpkgs.lib.nixosSystem {
           system = system;
           specialArgs = { inherit sddm-theme grub2-theme; };
           modules = [
@@ -141,51 +113,6 @@
               };
               home-manager.users.dreamer = { ... }: {
                 imports = [ ./profiles/dreamer/default.nix ];
-              };
-            }
-          ];
-        };
-        xneoncity = nixpkgs.lib.nixosSystem {
-          system = system;
-          specialArgs = { inherit sddm-theme grub2-theme; };
-          modules = [
-            ./machines/thinkpad-t420/xt420.nix
-            ./development/global.nix
-            ./ui/sddm.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-t420
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit
-                  neovim-nightly zig-master helix-master;
-              };
-              home-manager.users.dreamer = { ... }: {
-                imports = [ ./profiles/xdreamer/default.nix ];
-              };
-            }
-          ];
-        };
-
-        nc-wl = nixpkgs.lib.nixosSystem {
-          system = system;
-          specialArgs = { inherit sddm-theme grub2-theme; };
-          modules = [
-            ./machines/thinkpad-t420/t420-wl.nix
-            ./development/global.nix
-            ./ui/sddm.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-t420
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit
-                  neovim-nightly zig-master helix-master;
-              };
-              home-manager.users.dreamer = { ... }: {
-                imports = [ ./profiles/dreamer-wl/default.nix ];
               };
             }
           ];
