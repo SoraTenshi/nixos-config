14c5881f503c96401895197d251b285e84dddf5f
Sora <dream@neoncity.dev>
2022-10-14 13:08:49 +0200

    workaround for monitor setup
    a

--- ui/x11/xserver/battlestation.nix
+++ ui/x11/xserver/battlestation.nix
@@ -1,10 +1,23 @@
 { pkgs, ... }:
 {
+  services.autorandr.enable = true;
   services.xserver = {
-    videoDrivers = [ "nvidia" ];
-    deviceSection = ''
-      Option "TearFree" "true"
+    displayManager.sessionCommands = ''
+      xrandr --output DVI-D-0 --off \
+             --output HDMI-0 --mode 1920x1080 --pos 990x0 --rate 60 --rotate normal \
+             --output DP-0 --rate 165 --primary --mode 1920x1080 --pos 0x1080 --rotate normal \
+             --output DP-1 --off \
+             --output DP-2 --off \
+             --output DP-3 --off \
+             --output DP-4 --mode 1920x1080 --pos 1920x1080 --rate 75 --rotate normal --output DP-5 --off
     '';
+  
+    synaptics = {
+      minSpeed = "0.2";
+      accelFactor = null;
+    };
+
+    videoDrivers = [ "nvidia" ];
     xrandrHeads = [
       {
         output = "DP-0";
