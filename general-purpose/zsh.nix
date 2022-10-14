80469e2c84cf293e073887b3396c1e3545844d15
Sora <dream@neoncity.dev>
2022-10-14 18:51:28 +0200

    abc

--- general-purpose/zsh.nix
+++ general-purpose/zsh.nix
@@ -9,6 +9,7 @@ let
     mv       = "mv -i ";
     ls       = "exa --icons ";
     cd       = "z ";
+    termbin  = "nc termbin.com 9999"; 
   };
 in
 {
@@ -36,6 +37,12 @@ in
     initExtra = ''
       eval "$(zoxide init zsh)"
       eval "$(oh-my-posh init zsh --config '/home/dreamer/.oh-my-posh/tokyonight_ascii.opm.json')"
+
+      export KEY_SHIFT_CMD_LEFT=$'^[[1;10D'
+      export KEY_SHIFT_CMD_RIGHT=$'^[[1;10C'
+      export KEY_DEL=$'^[[3~' # Del
+      export KEY_BACKSPACE=$'^?' # Backspace
+      
     '';
 
     shellAliases = aliases;
