edc88401040886d16d62511508cc11e370d8a310
SoraTenshi <dream@neoncity.dev>
2022-11-20 14:29:58 +0100

    Remove nuissance called betterlockscreen
    a

--- ui/x11/betterlockscreen.nix
+++
@@ -1,8 +0,0 @@
-{ self, pkgs, ... }:
-{
-  services.betterlockscreen = {
-    enable = true;
-    arguments = [ "-u" "${self}/images/background.png" "--blur 0.5" "-l" "--dim 60" "-w"];
-    inactiveInterval = 10;
-  };
-}
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -39,7 +39,6 @@ ovrModMask = mod4Mask
 rofiShowRun = spawn "dmenu_run -l 15 -fn 'Lilex Nerd Font-16' -nb '#24283b' -nf '#a9b1d6' -sb '#414868' -sf '#7aa2f7' -p '-> ' &"
 rofiShowShutdown = spawn "rofi -show menu -modi 'menu:rofi-power-menu' &"
 networkManager = spawn "networkmanager_dmenu -l 15 -fn 'Lilex Nerd Font-16' -nb '#24283b' -nf '#a9b1d6' -sb '#414868' -sf '#7aa2f7' &"
-lockScreen = spawn "betterlockscreen -l --blur 0.5 --dim 60"
 screenshot = spawn "flameshot gui &"
 
 
@@ -51,7 +50,6 @@ ovrKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_p     ), rofiShowShutdown)
     , ((modm .|. shiftMask, xK_q     ), kill)
     , ((modm,               xK_space ), sendMessage NextLayout)
-    , ((modm .|. shiftMask, xK_l     ), lockScreen)
     , ((modm .|. shiftMask, xK_n     ), refresh)
     , ((controlMask,        xK_Print ), screenshot)
     , ((modm,               xK_r     ), sendMessage $ Rotate)
