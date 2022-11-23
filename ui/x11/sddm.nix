008ce151ddc85bad0bee1649728e021f300c486d
SoraTenshi <dream@neoncity.dev>
2022-11-23 20:06:51 +0100

    Update attempt?

--- ui/x11/sddm.nix
+++ ui/x11/sddm.nix
@@ -6,6 +6,10 @@
     style         = "gtk2";
   };
 
+	environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtgraphicaleffects
+	];
+
   services.xserver.displayManager = {
     sddm = {
       enable = true;
