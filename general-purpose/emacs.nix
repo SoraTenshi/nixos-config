75c8d6ece61ffcbab55f32dd46ab25c7f09d2df8
SoraTenshi <dream@neoncity.dev>
2022-12-16 18:08:01 +0100

    Enable nix-mode

--- general-purpose/emacs.nix
+++ general-purpose/emacs.nix
@@ -1,11 +1,8 @@
 { self, pkgs, ... }:
 {
   # Necessary plugins:
-  # - meow-edit
   # - zig-mode
-  # - tokyo-night-storm
   # - rainbow indentation guides (need to be figured out)
-  # - rainbow brackets
   programs.emacs = {
     enable = true;
     package = pkgs.emacsGit-nox;
@@ -51,6 +48,11 @@
             (nyan-start-animation)
           '';
           extraConfig = ":if window-system";
+        };
+        
+        nix-mode = {
+          enable = true;
+          hook = [ "(nix-mode . subword-mode)" ];
         };
       };
     };
