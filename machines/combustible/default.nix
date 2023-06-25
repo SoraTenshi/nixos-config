75fec475af1c7c99ddef9838a2417d1b9cdcc677
SoraTenshi <dream@neoncity.dev>
2023-06-25 20:31:17 +0200

    Up

--- machines/combustible/default.nix
+++ machines/combustible/default.nix
@@ -10,11 +10,9 @@
 
   programs.zsh.enable = true;
   programs.zsh.shellInit = ''
-    # Nix
     if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
       . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
     fi
-    # End Nix
   '';
 
   environment.shells = [ pkgs.bashInteractive pkgs.zsh ];
