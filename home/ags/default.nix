db567cdbd2a1703ae4e827d93d7f43e07120bd03
SoraTenshi <dream@neoncity.dev>
2024-05-10 19:07:52 +0200

    Up

--- home/ags/default.nix
+++ home/ags/default.nix
@@ -1,13 +1,12 @@
 { ags-env, pkgs, ... }: {
   home.packages = [
     pkgs.bun # Requirement for typescript ;)
-    pkgs.weather-icons # The weather icons for the weather display
     pkgs.material-symbols # Generalized symbols for all sorts of display ;)
   ];
 
   programs.ags = {
     enable = true;
-    configDir = ags-env;
+    # configDir = ags-env;
     extraPackages = [ pkgs.libsoup_3 ];
   };
 }
