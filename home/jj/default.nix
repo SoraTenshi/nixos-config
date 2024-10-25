0321e15f123273e5d25f8872fc8f0b20ac8ae629
SoraTenshi <dream@neoncity.dev>
2024-10-26 01:42:58 +0200

    Use homemanger module for jj

--- home/jj/default.nix
+++ home/jj/default.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }: {
-  home.packages = [
-    pkgs.jujutsu
-  ];
+_: {
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        name = "SoraTenshi";
+        email = "dream@neoncity.dev";
+      };
+      ui = {
+        editor = "hx";
+        paginate = "never";
+      };
+      signing = {
+        sign-all = true;
+        backend = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
+      };
+    };
+  };
 }
