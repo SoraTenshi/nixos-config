cce2760ea63177eb1687addaca45d34fd2ac96a2
SoraTenshi <dream@neoncity.dev>
2024-10-30 11:43:14 +0100

    uhh...

--- home/development/common-lisp.nix
+++ home/development/common-lisp.nix
@@ -1,20 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+let
+  sbcl-env = (pkgs.symlinkJoin {
+      name = "sbcl-env";
+      paths = with pkgs.sbclPackages; [
+        # CLI Parser
+        clingon
+
+        # CLI stuff
+        trivial-shell
+
+        # Coloured output!
+        cl-ansi-term
+
+        # Strings & dependencies
+        str cl-ppcre cl-ppcre-unicode cl-ppcre-template cl-unicode
+        ];
+      });
+in
+{
   home.packages = [
-    (pkgs.symlinkJoin {
-    name = "sbcl-env";
-    paths = with pkgs.sbclPackages; [
-      # CLI Parser
-      clingon
-
-      # CLI stuff
-      trivial-shell
-
-      # Coloured output!
-      cl-ansi-term
-
-      # Strings
-      str
-      ];
-    })
+    sbcl-env
   ];
+
+  home.file = {
+    "common-lisp/sbcl-env".source = sbcl-env;
+  };
 }
