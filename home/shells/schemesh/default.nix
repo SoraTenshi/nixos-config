cde0e6f28f2b705318fec9073366913390a6c68d
SoraTenshi <dream@neoncity.dev>
2025-05-04 02:54:43 +0200

    fix schemesh

--- home/shells/schemesh/default.nix
+++ home/shells/schemesh/default.nix
@@ -2,12 +2,11 @@
 let
   schemesh = pkgs.fetchgit {
     url = "https://github.com/cosmos72/schemesh.git";
-    rev = "refs/tags/v0.9.0"; # or: "refs/heads/main"
-    sha256 = ""; # insert sha256 when ready
+    rev = "refs/tags/v0.9.0";
+    sha256 = "sha256-rNmNpUXsOAsJwSBwmS6XCOMQ4+7lilkteJ8Or9bd+zc=";
   };
 in {
-  environment.systemPackages = [
-    # ...
+  home.packages = [
     pkgs.chez
     (pkgs.callPackage schemesh {})
   ];
