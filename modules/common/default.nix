63153efca6c0e64fe26c8584fe83f37f25e19601
SoraTenshi <dream@neoncity.dev>
2025-05-14 15:43:34 +0200

    enable usb tethering (shoutout to telecom)

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -5,12 +5,21 @@
     enableNg = true;
   };
 
+  services.usbmuxd = {
+    enable = true;
+    package = pkgs.usbmuxd2;
+  };
+
   environment.sessionVariables = {
     NH_FLAKE = "/home/${username}/dev/nixos-config";
     STEEL_HOME = "/home/${username}/.config/steel";
   };
 
   environment.systemPackages = with pkgs; [
+    # phone
+    libimobiledevice
+    ifuse
+    # 
     nh
     nvd
     nix-output-monitor
