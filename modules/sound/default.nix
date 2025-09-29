1c5ce013f59220f7492dd7a4f1437be6396bf2b5
SoraTenshi <dream@neoncity.dev>
2025-09-29 16:11:02 +0200

    add easyeffects

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ pavucontrol ];
+  environment.systemPackages = with pkgs; [ pavucontrol easyeffects ];
   services.pipewire = {
     enable = true;
     alsa.enable = true;
