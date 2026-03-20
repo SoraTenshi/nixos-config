f993841466f495c05f2642dd6c6636b0977c9ab7
SoraTenshi <dream@neoncity.dev>
2026-03-20 13:05:07 +0100

    emacs and flatpak stuff

--- home/editors/emacs/default.nix
+++ home/editors/emacs/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }: {
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs-pgtk;
+    defaultEditor = true;
+    startWithUserSession = true;
+  };
+
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-pgtk;
--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -26,6 +26,7 @@ in
     };
     packages = [
       # "app.zen_browser.zen"
+      "com.github.tchx84.Flatseal"
     ];
   };
 }
